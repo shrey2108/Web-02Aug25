a10da80745588de16f7034ce10df77f9f479f7aa
shrey2108 <shreyancebodh@gmail.com>
2025-10-12 18:45:36 +0530

    db relationships, ecom backend setup, folder struture, router, controllers, models

---
+++ Lec-19/e-com/models/user.model.js
@@ -0,0 +1,27 @@
+const mongoose = require("mongoose");
+// authentication and authorization (later)
+
+const userSchema = mongoose.Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  email: {
+    type: String,
+    unique: true,
+    required: true
+  },
+  role: {
+    type: String,
+    enum: ["buyer", "seller"],
+    required: true
+  },
+
+  // createdAt: timestamp
+  // updatedAt: timestamp
+}, {
+  timestamps: true
+})
+
+const User = mongoose.model("User", userSchema);
+module.exports = User;
---
+++ Lec-19/e-com/routes/product.routes.js
@@ -0,0 +1,7 @@
+const router = require("express").Router();
+
+router.get("/products", (req, res) => {
+
+});
+
+module.exports = router;
